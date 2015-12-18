e7a7ad115183633123e5594a0b92097efa279fae
Kat Burton <ka.bton@gmail.com>
2015-12-17 22:22:21 -0600

    Fixing completed cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -2,28 +2,10 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL
-    set <- function(y) {
-        x <<- y
-        inv <<- NULL
-    }
-    get <- function() x
-    setinv <- function(inverse) inv <<- inverse
-    getinv <- function() inv
-    list(set = set, get = get,
-         setinv = setinv,
-         getinv = getinv)
-
-}## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-## set takes the input and "sets" the matrix for the function
-## get brings back the matrix
-## inv set to NULL creates 'inv' slot/variable
-
+## 1. takes a matrix as an argument
+## 2. gets the value of that matrix
+## 3. solves the matrix to get the inverse
+## 4. set the inverse of the matrix so it can be easily retrieved
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -55,22 +37,3 @@ cacheSolve <- function(x, ...) {
     x$setinv(inv)
     inv
 }
-
-
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    inv <- x$getinv()
-    if(!is.null(inv)){
-        message("getting cached data")
-        return(inv)
-    }
-    data <- x$get()
-    inv <- solve(data, ...)
-    x$setinv(inv)
-    inv
-}
-
